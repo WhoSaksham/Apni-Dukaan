dbedd94fa78cdb197fc2611d2297fea850bb5e46
Saksham <sakshamag.work@gmail.com>
2023-08-04 17:40:08 +0530

    Done Changes for Deployment

--- apni_dukaan_BE/src/db.js
+++ apni_dukaan_BE/src/db.js
@@ -2,8 +2,10 @@ const { connect } = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // await connect('mongodb://127.0.0.1:27017/ApniDukaan');
-        await connect(process.env.MONGO_URI);
+        await connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log("Can't connect to MongoDB", error.message);
